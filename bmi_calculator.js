c16f069f43deccb7094abc6ddbc777a06700ccdc
Merlin R Janoras <quagmire1224@gmail.com>
2023-03-11 00:07:33 +0800

    First New Assignment: BMI Calculator

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -1,7 +1,7 @@
 /* Calculates Body Mass Index (BMI)
  * weight in kilograms
  * height in meters
- * returns the BMI
+ * returns the BMIgit 
  */
 function calculateBMI(weight, height) {
     return weight / (height*height);
